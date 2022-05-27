9f3afd63eff6e81b588548e16fa44f59fa555a88
babiq <2304092386@qq.com>
2022-05-27 16:07:25 +0800

    text commit

--- hm_04.py
+++ hm_04.py
@@ -2,6 +2,13 @@ poem = ["昔人已乘黄鹤去,此地空余黄鹤楼。",
         "黄鹤一去不复返,白云千载空悠悠。",
         "晴川历历汉阳树，芳草萋萋鹦鹉洲。",
         "日暮乡关何处是？烟波江上使人愁。"]
+print(poem)
 
 for poem_str in poem:
     print("|%s|" % poem_str.strip().center(10, "　"))
+
+# 拆分
+poem_list = poem_str.split()
+print(poem)
+# 合并
+print("".join(poem))
